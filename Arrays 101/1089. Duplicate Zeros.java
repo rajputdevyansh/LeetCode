72f85b3d1a4fe8f0d00b4f898f9c7d3d2fa02243
Devyansh Rajput <devyanshrajput50@gmail.com>
2023-08-18 01:51:33 +0530

    1089

---
+++ Arrays 101/1089. Duplicate Zeros.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] duplicateZeros(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 0){
+                for(int j = arr.length - 1; j > i; j--){
+                    arr[j] = arr[j - 1];
+                }
+                i++;
+            }
+        }
+        return arr;
+    }
+}
